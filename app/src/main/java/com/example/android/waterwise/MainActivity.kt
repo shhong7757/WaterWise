2670d94750966fbd269a55ecd6452ce474a59dec
shhong7757 <ghdtkdgur123@gmail.com>
2023-07-14 00:00:26 +0900

    feat: navigation bar 추가
    의존성 추가
    
    material3
    navigation-compose
    
    ---
    
    material3 사용을 위해 compileSdk, targetSdk 33으로 수정
    
    ---
    
    ui 패키지 아래 screen 패키지 추가 및 Home 파일 추가
    navHost의 startDestination을 Home으로 설정

--- app/src/main/java/com/example/android/waterwise/MainActivity.kt
+++ app/src/main/java/com/example/android/waterwise/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.android.waterwise
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.android.waterwise.ui.screen.HomeScreen
 import com.example.android.waterwise.ui.theme.WaterWiseTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +24,55 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             WaterWiseTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                Navigation()
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Navigation() {
+    val navController = rememberNavController()
+    Scaffold(
+        bottomBar = {
+            BottomNavigation(navController = navController)
+        }
+    ) { padding ->
+        Column(
+            modifier = Modifier
+                .padding(padding)
+        ) {
+            NavigationGraph(navController = navController)
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    WaterWiseTheme {
-        Greeting("Android")
+fun NavigationGraph(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = "home") {
+        composable("home") { HomeScreen(navController) }
+    }
+}
+
+@Composable
+fun BottomNavigation(navController: NavController) {
+    var selectedItem by remember { mutableStateOf(0) }
+    val items = listOf("Home")
+
+    NavigationBar {
+        items.forEachIndexed { index, item ->
+            NavigationBarItem(
+                icon = { Icon(Icons.Filled.Favorite, contentDescription = item) },
+                label = { Text(item) },
+                selected = selectedItem == index,
+                onClick = {
+                    selectedItem = index
+                    when (index) {
+                        0 -> navController.navigate("home")
+                    }
+                }
+            )
+        }
     }
 }
---
+++ app/src/main/java/com/example/android/waterwise/ui/screen/Home.kt
@@ -0,0 +1,10 @@
+package com.example.android.waterwise.ui.screen
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun HomeScreen(navController: NavController) {
+    Text(text = "Home")
+}
