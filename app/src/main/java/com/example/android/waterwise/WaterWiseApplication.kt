f605dd1f7f6c440091bc3bf107b8020c8c677276
shhong7757 <ghdtkdgur123@gmail.com>
2023-07-16 12:18:28 +0900

    chore: Hilt 의존성 추가

--- app/src/main/java/com/example/android/waterwise/WaterWiseApplication.kt
+++ app/src/main/java/com/example/android/waterwise/WaterWiseApplication.kt
@@ -5,11 +5,13 @@ import android.content.Context
 import androidx.datastore.preferences.preferencesDataStore
 import com.example.android.waterwise.data.AppContainer
 import com.example.android.waterwise.data.AppDataContainer
+import dagger.hilt.android.HiltAndroidApp
 
 private const val USER_PREFERENCES_NAME = "user_preferences"
 
 private val Context.dataStore by preferencesDataStore(name = USER_PREFERENCES_NAME)
 
+@HiltAndroidApp
 class WaterWiseApplication : Application() {
     lateinit var container: AppContainer
 
